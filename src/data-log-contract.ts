31d185299e2d5e73fbd5037520b71717f11b5b2a
juzhiqiang <1020814597@qq.com>
2025-08-27 21:55:45 +0800

    fix： 修复乱码问题

--- src/data-log-contract.ts
+++ src/data-log-contract.ts
@@ -1,46 +1,53 @@
+import { Bytes } from "@graphprotocol/graph-ts";
 import {
   BatchDataStored as BatchDataStoredEvent,
   DataStored as DataStoredEvent,
-  OwnershipTransferred as OwnershipTransferredEvent
-} from "../generated/DataLogContract/DataLogContract"
+  OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/DataLogContract/DataLogContract";
 import {
   BatchDataStored,
   DataStored,
-  OwnershipTransferred
-} from "../generated/schema"
+  OwnershipTransferred,
+} from "../generated/schema";
 
 export function handleBatchDataStored(event: BatchDataStoredEvent): void {
   let entity = new BatchDataStored(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.logIds = event.params.logIds
-  entity.creator = event.params.creator
-  entity.dataType = event.params.dataType.toString()
-  entity.timestamp = event.params.timestamp
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let dataTypeBytes = event.params.dataType as Bytes;
+  entity.dataType = dataTypeBytes.toHexString();
 
-  entity.save()
+  entity.logIds = event.params.logIds;
+  entity.creator = event.params.creator;
+  entity.timestamp = event.params.timestamp;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleDataStored(event: DataStoredEvent): void {
   let entity = new DataStored(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.logId = event.params.logId
-  entity.creator = event.params.creator
-  entity.dataType = event.params.dataType.toString()
-  entity.content = event.params.content
-  entity.timestamp = event.params.timestamp
-  entity.dataHash = event.params.dataHash
+  );
+  entity.logId = event.params.logId;
+  entity.creator = event.params.creator;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  let dataTypeBytes = event.params.dataType as Bytes;
+  entity.dataType = dataTypeBytes.toHexString();
 
-  entity.save()
+  entity.content = event.params.content;
+  entity.timestamp = event.params.timestamp;
+  entity.dataHash = event.params.dataHash;
+
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -48,13 +55,12 @@ export function handleOwnershipTransferred(
 ): void {
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+  );
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  entity.save();
 }
